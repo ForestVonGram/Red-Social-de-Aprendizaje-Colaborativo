456a02382dc9102995a3536a8761c6803a678ec2
DAVID GR <playa200612@gmail.com>
2025-05-07 08:18:59 -0500

    Enlazado de LoginPage y RegisterPage. Corrección del fondo de la página de registro.

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,12 +1,16 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Landing from "@/pages/LandingPage";
+import LoginPage from "@/pages/LoginPage";
+import RegisterPage from "@/pages/RegisterPage";
 
 export default function AppRoutes() {
     return (
         <Router>
             <Routes>
                 <Route path="/" element={<Landing />} />
-                {/* Puedes agregar más rutas aquí */}
+                <Route path="/LoginPage" element={<LoginPage />} />
+                <Route path="/RegisterPage" element={<RegisterPage />} />
+                {/* Aquí van las rutas */}
             </Routes>
         </Router>
     );
